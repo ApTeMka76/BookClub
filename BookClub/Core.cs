9c2a10a6a6b2677f295d1325d510f789ef56f60a
ApTeMk@ <zhma@mail.ru>
2023-06-07 19:02:59 +0300

    Добавьте файлы проекта.

---
+++ BookClub/Core.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClub
+{
+    public class Core
+    {
+        private static Data.BookClubEntities _context;
+        public static Data.BookClubEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new Data.BookClubEntities();
+            }
+            return _context;
+        }
+    }
+}
